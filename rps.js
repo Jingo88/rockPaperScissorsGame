5395ed80c042016073e8915d28b2624f02d5f716
Jingo88 <jcory.ng@gmail.com>
2015-01-12 15:17:18 -0500

    made a few edits for DRY and UX

--- rps.js
+++ rps.js
@@ -20,35 +20,33 @@ var draw = 0;
 	function fire(){
 			userPick = "fire";
 			$('.pictures').hide();
+			clear();
 			compPick = compChoice();
-			console.log(compPick);
 			$('.userelements .imgfire').show();
 			compare(userPick, compPick);	
-			clear();
 	}
 
 	function water() {
 			userPick = "water";
 			$('.pictures').hide();
+			clear();
 			compPick = compChoice();
-			console.log(compPick);
 			$('.userelements .imgwater').show();
 			compare(userPick, compPick);
-			clear();
 	}
 
 	function earth() {
 			userPick = "earth";
 			$('.pictures').hide();
+			clear();
 			compPick = compChoice();
-			console.log(compPick);
 			$('.userelements .imgearth').show();
 			compare(userPick, compPick);
 	}
 
+//returns a random computer choice between the elements.
 function compChoice(){
-		compPick = Math.floor(Math.random()*elements.length);
-		compPick = elements[compPick];
+		compPick = elements[Math.floor(Math.random()*elements.length)];
 		return compPick
 	}
 
@@ -61,6 +59,7 @@ function reset(){
 	 win = 0;
 	 lose = 0;
 	 draw = 0;
+	 clear();
 }
 
 function compare(user, comp){
@@ -72,28 +71,33 @@ function compare(user, comp){
 			$('.compelements .imgwater').show();
 			alert('You Lose: Let off some steam Bennett');
 			lose++;
+
 		} else if (userPick == "fire" && compPick == "earth") {
 			$('.compelements .imgearth').show();
 			alert('You Win: There was a fire fight!');
 			win++;
+
 		} else if (userPick == "water" && compPick == "fire") {
 			$('.compelements .imgfire').show();
 			alert('You Win: Cool em off');
 			win++;
+
 		} else if (userPick == "water" && compPick == "earth"){
 			$('.compelements .imgearth').show();
 			alert('You Lose: Stop feeding them');
 			lose++; 
+
 		} else if (userPick == "earth" && compPick == "water") {
 			$('.compelements .imgwater').show();
 			alert('You Win: You are very hydrated');
 			win++;
+
 		} else if (userPick == "earth" && compPick == "fire") {
 			$('.compelements .imgfire').show();
 			alert('You Lose: The roof is on fire');
 			lose++;
+
 		} else if (userPick == compPick) {
-		//How to make both images show when it's a tie?!?!?!
 			
 			if (userPick == "fire") {
 				$('.compelements .imgfire').show();
